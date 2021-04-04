686b75dfc7d838d423f567438f3180fd24529463
BrendanJHarris <brendanjohnharris@gmail.com>
2021-04-04 21:36:29 +1000

    Window view for time series

--- src/Plotting.jl
+++ src/Plotting.jl
@@ -37,7 +37,7 @@ import StatsBase
 # This one takes a time series and moves all the way through to esimation, plotting relevant stuff
 # Plots best in pyplot()
 @userplot ParameterEstimate
-@recipe function f(P::ParameterEstimate; cluster=false)
+@recipe function f(P::ParameterEstimate; cluster=false, tswindows=(length(P.args[1].timeseries) > 10000))
     I = P.args[1]
     legend --> false
     #link := :x
@@ -51,18 +51,53 @@ import StatsBase
     left_margin --> 5Plots.mm
     right_margin --> 15Plots.mm
 
-    windowCentres = (I.windowEdges[1:end-1] + I.windowEdges[2:end])./2
-
+    windowCentres = round.((I.windowEdges[1:end-1] + I.windowEdges[2:end])./2)
 # ------------------------------------------ Time series ----------------------------------------- #
-
-    @series begin
-        seriestype := :path
-        subplot := 1
-        seriescolor := :black
-        x = I.timeseries
+    t = dims(I.timeseries, Ti).val
+    if tswindows
+        @series begin
+            seriestype := :tswindows
+            subplot := 1
+            yguide --> "Time Series"
+            xguide := ""
+            framestyle := :box
+            yaxis --> nothing
+            xaxis --> nothing
+            top_margin --> 3Plots.mm
+            ymax = round(max(I.timeseries...),  sigdigits=2)
+            ymin = round(min(I.timeseries...),  sigdigits=2)
+            annotations := [(max(t...)*1.01, ymin, text("$ymin", :black, :left, 8)),
+            (max(t...)*1.01, ymax, text("$ymax", :black, :left, 8))]
+            xlims := extrema(t)
+            (t, I.timeseries)
+        end
+    else
+        @series begin
+            seriestype := :path
+            subplot := 1
+            yguide --> "Time Series"
+            xguide := ""
+            framestyle := :box
+            yaxis --> nothing
+            xaxis --> nothing
+            top_margin --> 3Plots.mm
+            seriescolor := :black
+            ymax = max(I.timeseries...)
+            ymin = min(I.timeseries...)
+            ymax -= 0.05*(ymax-ymin)
+            ymin += 0.05*(ymax-ymin)
+            ymax = round(ymax,  sigdigits=2)
+            ymin = round(ymin,  sigdigits=2)
+            annotations:= [(max(t...)*1.01, ymin, text("$ymin", :black, :left, 8)),
+            (max(t...)*1.01, ymax, text("$ymax", :black, :left, 8))]
+            xlims := extrema(t)
+            x := t
+            y := I.timeseries
+        end
     end
 
 # ------------------------------------------- Features ------------------------------------------- #
+    # Need to edit the plot attributes of this subplot in the last series of this recipe
     F = I.normalisation(I.F)
     if cluster
         #D = StatsBase.corspearman(F')
@@ -78,15 +113,12 @@ import StatsBase
         seriestype := :heatmap
         subplot := 2
         seriescolor := cgrad(:RdYlBu_11, 7, categorical = true)
-        yticks := nothing
         println("The clustered features are:\n")
         display.(dims(F, :feature).val[idxs])
         #clusterReorder(F, CorrDist(), linkageMetric=:average, branchOrder=:optimal, dim=1)
-
-
-        colorbar --> nothing
         (x, y, X) = (dims(I.timeseries, Ti).val[Int.(windowCentres)], 1:size(clusterF, 1), sigmoidNormalise(clusterF))
     end
+
 
 
 # ------------------------------------------ Parameters ------------------------------------------ #
@@ -96,13 +128,17 @@ import StatsBase
         subplot := 3
         yaxis := nothing
         seriescolor := :black
-        (x, y) = (dims(I.timeseries, Ti).val, I.parameters)
+        xlims := extrema(t)
+        (x, y) = (t, I.parameters)
     end
 
+    inset_subplots := (3, bbox(0,0,1,1))
     @series begin
         seriestype := :scatter
         seriescolor := :black
-        inset_subplots := (3, bbox(0,0,1,1))
+        xguide --> "Time"
+        yguide --> "Parameters"
+        framestyle := :box
         yaxis := nothing
         xaxis := nothing
         background_color_inside := nothing
@@ -112,73 +148,50 @@ import StatsBase
             @warn "It looks like the estimated parameters are the negative of the true parameters. This is not unexpected, so flipping for visualisation."
             p = -p
         end
+        ymax = max(p...)
+        ymin = min(p...)
+        ymax -= 0.05*(ymax-ymin)
+        ymin += 0.05*(ymax-ymin)
+        ylims := (ymin, ymax)
+        ymax -= 0.05*(ymax-ymin)
+        ymin += 0.05*(ymax-ymin)
+        ymax = round(ymax,  sigdigits=2)
+        ymin = round(ymin,  sigdigits=2)
+        xs = length(p)*1.01
+        annotations:= [(xs, ymin, text("$ymin", :black, :left, 8)),
+        (xs, ymax, text("$ymax", :black, :left, 8))]
         xlims := (0, length(p)) # These go in centres of windows
 
         (x, y) = (0.5:1:length(p)-0.5, p)
     end
 
-
-# ---------------------------- Add windows; how to remove boilerplate? --------------------------- #
-# Edit most axes properties here
-
-    @series begin
-        seriescolor := nothing
-        seriestype := :vline
-        inset_subplots := (1, bbox(0,0,1,1))
-        yaxis := nothing
-        xaxis := nothing
-        yguide --> "Time Series"
-        framestyle := :box
-        background_color_inside := nothing
-        background_color_subplot := nothing
-        ymax = round(max(I.timeseries...),  sigdigits=2)
-        ymin = round(min(I.timeseries...),  sigdigits=2)
-        annotations:= [(max(dims(I.timeseries, Ti).val...)*1.01, ymin, text("$ymin", :black, :left, 8)),
-        (max(dims(I.timeseries, Ti).val...)*1.01, ymax, text("$ymax", :black, :left, 8))]
-        xlims := extrema(dims(I.timeseries, Ti).val)
-        x = dims(I.timeseries, Ti).val[I.windowEdges]
-    end
+# -------------------------------- Add windows to the feature plot ------------------------------- #
     @series begin
         seriescolor := :black
         seriestype := :vline
-        inset_subplots := (2, bbox(0,0,1,1))
-        yaxis := nothing
-        xaxis := nothing
-        clims := (0.0, 1.0)
-        yguide --> "Features"
+        subplot := 2
+        framestyle := :box
+        colorbar --> nothing
+        ymax = length(ρ)+0.5
+        ymin = 0.5
+        ymax -= 0.05*(ymax-ymin)
+        ymin += 0.05*(ymax-ymin)
         if cluster == false
             ρmin = round(min(ρ...), sigdigits=2)
             ρmax = round(max(ρ...), sigdigits=2)
-            annotations:= [(max(dims(I.timeseries, Ti).val...)*1.01, length(ρ)-0.5, text("ρ = $ρmin", :black, :left, 8)),
-                        (max(dims(I.timeseries, Ti).val...)*1.01, 1.0, text("ρ = $ρmax", :black, :left, 8))]
+            annotations:= [(max(dims(I.timeseries, Ti).val...)*1.01, ymax, text("ρ = $ρmin", :black, :left, 8)),
+                        (max(dims(I.timeseries, Ti).val...)*1.01,  ymin, text("ρ = $ρmax", :black, :left, 8))]
         end
+        yaxis := nothing
+        xaxis := nothing
         yflip := true
+        yguide --> "Features"
         framestyle := :box
-        background_color_inside := nothing
-        background_color_subplot := nothing
-        xlims := extrema(dims(I.timeseries, Ti).val)
+        #xlims := extrema(dims(I.timeseries, Ti).val)
         ylims := (0.5, length(idxs)+0.5)
         x = dims(I.timeseries, Ti).val[I.windowEdges]
     end
 
-    @series begin
-        seriescolor := nothing
-        seriestype := :vline
-        inset_subplots := (3, bbox(0,0,1,1))
-        yaxis := nothing
-        #foreground_color_axis := nothing
-        xguide --> "Time"
-        yguide --> "Parameters"
-        framestyle := :box
-        background_color_inside := nothing
-        background_color_subplot := nothing
-        ymax = round(max(I.parameters...),  sigdigits=2)
-        ymin = round(min(I.parameters...),  sigdigits=2)
-        annotations:= [(max(dims(I.timeseries, Ti).val...)*1.01, ymin, text("$ymin", :black, :left, 8)),
-        (max(dims(I.timeseries, Ti).val...)*1.01, ymax, text("$ymax", :black, :left, 8))]
-        xlims := extrema(dims(I.timeseries, Ti).val)
-        x = dims(I.timeseries, Ti).val[I.windowEdges]
-    end
 end
 
 
@@ -220,5 +233,48 @@ end
         yguide --> "Explained Variance"
         (x, y) = (0:length(ξ²), [0, ξ²...])
     end
+end
 
+
+
+
+
+@recipe function f(::Type{Val{:tswindows}}, x, y, z; windows=5, N=min(length(x)÷10, 1000))
+    L = length(y)
+    xlims, ylims = (extrema(x), extrema(y))
+    scale = (ylims[2] - ylims[1])*0.1
+    ylims = (ylims[1] - scale, ylims[2] + scale)
+    legend --> false
+    ylims := ylims
+    #seriescolor --> :black
+    #grid --> false
+
+
+    # We want to take a long time series, choose some windows of length N and plot these adjacently
+    idxs = Int.(round.(LinRange(N÷2, L-N÷2, windows)))
+    idxs = [1+(i-N÷2):(i+N÷2) for i in idxs]
+    dispxs = LinRange(0, max(x...), length(vcat(idxs...)))
+    nn = -N+1
+    for s in 1:length(idxs)
+        nn += N
+        if s < length(idxs)
+            barx = (max(dispxs[nn:nn+N-1]...) + min(dispxs[(nn+N):(nn+2*N-1)]...))/2
+            @series begin
+                seriestype := :path
+                linewidth := 3
+                seriescolor := :red
+                x := [barx, barx]
+                y := [min(y...)-scale, max(y...)+scale]
+            end
+        end
+        @series begin
+            seriestype := :path
+            #framestyle --> :box
+            #xlims --> xlims
+            #ylims --> ylims
+            seriescolor := :black
+            x := dispxs[nn:nn+N-1]
+            y := y[idxs[s]]
+        end
+    end
 end
