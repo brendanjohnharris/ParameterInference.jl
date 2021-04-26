8f297eaad0ddc558e4cfa287c61e5593b4b0d0a0
BrendanJHarris <brendanjohnharris@gmail.com>
2021-04-26 13:23:18 +1000

    New baseline method: scale test matrix by variances mapped between low and high dim. baselines

--- src/Baseline.jl
+++ src/Baseline.jl
@@ -1,10 +1,11 @@
 using StatsBase
 import StatsBase.std
+using DimensionalData
 
 function reStandardise(F::AbstractArray)
     idxs = vec(nanrows(F) .| constantrows(F))
-    𝛔 = vec(StatsBase.std(F, dims=2))
-    𝛍 = vec(mean(F, dims=2))
+    𝛔 = StatsBase.std(F, dims=2)
+    𝛍 = mean(F, dims=2)
     𝛔[idxs] .= Inf
     return X -> normalise(X, 𝛍, 𝛔, standardise, 2)
 end
@@ -29,24 +30,41 @@ export reZero
 #                            Combine a high and low dimensional baseline                           #
 # ------------------------------------------------------------------------------------------------ #
 function reScale(x::AbstractVector, f::Function=_self)
-    σ = std(F)
+    σ = std(x)
     σ′ = f(σ)
     return σ′.*x
 end
-function reScale(F::AbstractArray, f::Vector{Function})
-    F′ = copy(F)
+function reScale(F::AbstractArray, f::Vector)
+    F′ = deepcopy(F)
     for r = 1:size(F, 1)
         F′[r, :] = reScale(F′[r, :] , f[r])
     end
-    return F′
+   return F′
 end
-function hiloScale(Fₗ::AbstractArray{Float64, 2}, Fₕ::AbstractArray{Float64, 2},
-                    interval::Function=(x, y) -> NonstationaryProcesses.intervalRamp(0, 1, x, y))
+function reScale(F::DimArray, f::DimArray{T, 1}) where {T}
+    (Fᵣ, fᵣ) = intersectFeatures(F, f) # Assumes fᵣ has all of the features of Fᵣ
+    reScale(Fᵣ, vec(fᵣ))
+end
+function hiloScale(Fₗ::Array{Float64, 2}, Fₕ::Array{Float64, 2},
+                    interval::Function=(x, y) -> NonstationaryProcesses.rampInterval(0, 1, x, y))
     # interval gives a function of σ, the test variance, with parameters σₗ and σₕ
     if size(Fₗ, 1) != size(Fₕ, 1)
         error("High and low dimensional baselines do not have the same number of features")
-    endrampInter
+    end
     𝛔ₗ, 𝛔ₕ = std(Fₗ, dims=2), std(Fₕ, dims=2)
-    𝐟 = interval.(𝛔ₗ, 𝛔ₕ)
-    return reScale.(F, 𝐟)
+    𝐟 = interval.(vec(𝛔ₗ), vec(𝛔ₕ))
+    return F -> reScale(F, 𝐟)
 end
+function hiloScale(Fₗ::DimArray{Float64, 2}, Fₕ::DimArray{Float64, 2},
+    interval::Function=(x, y) -> NonstationaryProcesses.rampInterval(0, 1, x, y))
+    # interval gives a function of σ, the test variance, with parameters σₗ and σₕ
+    if any(Catch22.featureDims(Fₗ) .!= Catch22.featureDims(Fₕ))
+        error("High and low dimensional baselines do not have the same features")
+    end
+    𝛔ₗ, 𝛔ₕ = std(Fₗ, dims=2), std(Fₕ, dims=2)
+    𝐟 = interval.(vec(𝛔ₗ), vec(𝛔ₕ))
+    𝐟 = Catch22.featureVector(𝐟, Catch22.featureDims(Fₗ))
+    return F -> reScale(F, 𝐟)
+end
+export hiloScale
+
